386113274fac1aa8fa4f47f93a7bc13543b12293
tnzlkhan <114957061+tanzeelkz@users.noreply.github.com>
2024-12-20 23:16:48 +0500

    Update RecordList.jsx

--- Application-Code/frontend/src/components/RecordList.jsx
+++ Application-Code/frontend/src/components/RecordList.jsx
@@ -41,7 +41,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://10.244.6.83:5050/record/`);
+      const response = await fetch(`http://10.244.3.166:5050/record/`);
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         console.error(message);
@@ -56,7 +56,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`http://10.244.6.83:5050/record/${id}`, {
+    await fetch(`http://10.244.3.166:5050/record/${id}`, {
       method: "DELETE",
     });
     const newRecords = records.filter((el) => el._id !== id);
